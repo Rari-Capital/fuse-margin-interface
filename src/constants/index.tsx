5bfa415e0ebae1baffc68fb50b359e1b715b93a0
Gautham Ganesh Elango <gautham.gg@gmail.com>
2021-04-22 14:10:58 +1000

    compound abis

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,1 +1,2 @@
-export const fusePoolDirectoryAddress: string = "0x835482FE0532f169024d5E9410199369aAD5C77E";
+export const fusePoolDirectoryAddress: string =
+  "0x835482FE0532f169024d5E9410199369aAD5C77E";
