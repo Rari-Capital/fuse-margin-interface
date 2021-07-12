9fa6d4db5070715c289e62a68e0e7bece0eaa849
Gautham Ganesh Elango <gautham.gg@gmail.com>
2021-07-12 19:51:32 +1000

    token selector

--- src/pages/trade/[pool].tsx
+++ src/pages/trade/[pool].tsx
@@ -1,43 +1,55 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useRouter } from "next/router";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Select, Center } from "@chakra-ui/react";
 import Layout from "../../components/layout";
 import Loading from "../../components/Loading";
 import { SelectPool } from "../../components/Trade";
 import useFuse from "../../hooks/useFuse";
-import useBalances from "../../hooks/useBalances";
-import useAllowances from "../../hooks/useAllowances";
 
 function Trade(): JSX.Element {
   const router = useRouter();
   const { pools } = useFuse();
   const pool: number = router.query.pool ? Number(router.query.pool) : 0;
-  const { balances } = useBalances(pool);
-  const { allowances } = useAllowances(
-    pool,
-    "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9"
-  );
+  const [token, setToken] = useState<number>(0);
 
   const setPool: (value: number) => void = useCallback(
-    (value: number) => router.push(`/trade/${value}`),
-    [router]
+    (value: number) => {
+      setToken(0);
+      router.push(`/trade/${value}`);
+    },
+    [router, setToken]
   );
 
-  console.log(
-    "Balances:",
-    balances.map((balance) => balance.toString())
-  );
-  console.log(
-    "Allowances:",
-    allowances.map((allowance) => allowance.toString())
-  );
+  const tokens = pools[pool]
+    ? pools[pool].assets.map((asset) => asset.underlyingSymbol)
+    : [];
+  const listItems = tokens.map((token, index) => (
+    <option key={token} value={index}>
+      {token}
+    </option>
+  ));
 
   return (
     <Layout>
       <Box p={2}>
         <Loading>
           {pool < pools.length ? (
-            <SelectPool fusePools={pools} pool={pool} setPool={setPool} />
+            <>
+              <SelectPool fusePools={pools} pool={pool} setPool={setPool} />
+              <Center>
+                <Select
+                  value={token}
+                  margin={1}
+                  onChange={(event) => setToken(Number(event.target.value))}
+                  maxW={100}
+                  size="md"
+                  color="black"
+                  bg="white"
+                >
+                  {listItems}
+                </Select>
+              </Center>
+            </>
           ) : (
             <Text>Invalid pool.</Text>
           )}
